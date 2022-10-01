87e24d72b38f84ab1ab0111cdb85b91eb6691913
DESKTOP-EH772EC\Yielonek <karol26688@gmail.com>
2022-10-01 13:05:03 +0200

    Adding the "Add Place" Screen & An Image Input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
-    static const routeName = '/add-place';
+  static const routeName = '/add-place';
 
   @override
   State<AddPlaceScreen> createState() => _AddPlaceScreenState();
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
-
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +17,34 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text('Add a New Place'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('User Inputs ...'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(), 
+                  ],
+                ),
+              ),
+            ),
+          ),
           RaisedButton.icon(
             icon: Icon(Icons.add),
             label: Text('Add place'),
             onPressed: (() {}),
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
           )
         ],
       ),
